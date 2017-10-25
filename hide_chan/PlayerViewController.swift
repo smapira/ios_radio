f168c58a07e8ba5e4ad83c23cf789c8f9d49a87d
smapira <info@routeflags.com>
2017-10-26 00:42:32 +0900

    Sound

--- hide_chan/PlayerViewController.swift
+++ hide_chan/PlayerViewController.swift
@@ -16,21 +16,14 @@ class PlayerViewController: AVPlayerViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-//        let url = URL(fileURLWithPath: "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8")
-////        let url = URL(fileURLWithPath: "http://yp.shoutcast.com/sbin/tunein-station.pls?id=1721052")
-//        let assets = AVAsset.init(url: url)
-//        let playerItem = AVPlayerItem.init(asset: assets)
-//        let player = AVPlayer(playerItem: playerItem)
-//        self.player = player
-//        self.player?.play()
-        
-        radioURL = URL(string: "https://devimages.apple.com.edgekey.net/samplecode/avfoundationMedia/AVFoundationQueuePlayer_HLS2/master.m3u8")
-        let player = AVPlayer(url: radioURL!)
-        let playerViewController = AVPlayerViewController()
-        playerViewController.player = player
-        self.present(playerViewController, animated: true) {
-            playerViewController.player!.play()
-        }
+		let playerViewController = AVPlayerViewController()
+		self.view.addSubview(playerViewController.view)
+		playerViewController.view.frame = self.view.frame
+		let radioURL = URL(string: "http://yp.shoutcast.com/sbin/tunein-station.pls?id=1721052")
+		let player = AVPlayer(url: radioURL!)
+		playerViewController.player = player
+		player.play()
+		
 	}
 	
 	override func didReceiveMemoryWarning() {
