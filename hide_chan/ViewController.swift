39b2ee0565b7b4010a177cac80c2eb7d14b10ac1
smapira <info@routeflags.com>
2017-10-25 14:20:20 +0900

    test

---
+++ hide_chan/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  hide_chan
+//
+//  Created by bookair17 on 10/25/17.
+//  Copyright © 2017 routeflags. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	var label: UILabel?
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+		label?.center = CGPoint(
+			x: self.view.frame.width  / 2,
+			y: self.view.frame.height / 2
+		)
+		label?.textAlignment = .center
+		label?.text = "Hello, World."
+		
+		self.view.addSubview(label!)
+		
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+}
+
---
+++ hide_chanTests/hide_chanTests.swift
@@ -0,0 +1,31 @@
+//
+//  hide_chanTests.swift
+//  hide_chanTests
+//
+//  Created by bookair17 on 10/25/17.
+//  Copyright © 2017 routeflags. All rights reserved.
+//
+
+import XCTest
+@testable import hide_chan
+
+class hide_chanTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+	
+    func testLabelTitle() {
+		let storyBoard = UIStoryboard(name: "Main", bundle: Bundle.main)
+		let viewController = storyBoard.instantiateViewController(withIdentifier: "viewController") as? ViewController
+		viewController?.viewDidLoad()
+		XCTAssertEqual(viewController?.label?.text, "Hello, World.")
+	}
+	
+}
