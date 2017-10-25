42560bc843119cf61fe9fcc6762fbdd1679803a5
KatsuyaEndoh <katsuya@amac.local>
2017-10-25 15:09:56 +0900

    test green

---
+++ hide_chan/Pla.swift
@@ -0,0 +1,38 @@
+//
+//  AVPlayerViewController.swift
+//  hide_chan
+//
+//  Created by bookair17 on 10/25/17.
+//  Copyright © 2017 routeflags. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class Pla: AVPlayerViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+//        let url = URL(fileURLWithPath: "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8")
+////        let url = URL(fileURLWithPath: "http://yp.shoutcast.com/sbin/tunein-station.pls?id=1721052")
+//        let assets = AVAsset.init(url: url)
+//        let playerItem = AVPlayerItem.init(asset: assets)
+//        let player = AVPlayer(playerItem: playerItem)
+//        self.player = player
+//        self.player?.play()
+        
+        let videoURL = URL(string: "https://devimages.apple.com.edgekey.net/samplecode/avfoundationMedia/AVFoundationQueuePlayer_HLS2/master.m3u8")
+        let player = AVPlayer(url: videoURL!)
+        let playerViewController = AVPlayerViewController()
+        playerViewController.player = player
+        self.present(playerViewController, animated: true) {
+            playerViewController.player!.play()
+        }
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+	}
+ 
+}
